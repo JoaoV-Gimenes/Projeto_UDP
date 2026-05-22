0ddd7c4b43d7f974f757abc8e4038b017d1acf4d
JoaoV-Gimenes <cavalotaticoveloz17@gmail.com>
2026-05-21 23:35:52 -0300

    Começo do código para treinamento

---
+++ treinamentos e data/treinamento.py
@@ -0,0 +1,9 @@
+## import das bibliotecas
+import json
+import os
+import numpy as np
+import tensorflow as tf
+import piclke
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
