0ca51ba537ac32e867de8ba317fa86c31376e7c2
JoaoV-Gimenes <cavalotaticoveloz17@gmail.com>
2026-05-25 22:57:10 -0300

    coleta de dados da detecção de rosto

--- treinamentos e data/coleta_dados.py
+++ treinamentos e data/coleta_dados.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 import os
 import json
 from mediapipe.tasks import python
-from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions, RunningMode
+from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions, RunningMode, FaceLandmarker, FaceLandmarkerOptions
 
 ## Caminho da pasta com os vídeos
 PASTA_DATASET = 'dataset'
@@ -15,25 +15,54 @@ SINAIS_DINAMICOS = ['H', 'J', 'K', 'X', 'Z']
 ## Onde salva os dados extraídos
 PASTA_SAIDA = 'dados_extraidos'
 
-## Config e detector para vídeo
-options_video = HandLandmarkerOptions(
+## Índices dos pontos principais do rosto
+CONTORNO = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109]
+LABIOS = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
+OLHO_ESQ = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
+OLHO_DIR = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+SOBRANCELHA_ESQ = [70, 63, 105, 66, 107, 55, 65, 52, 53, 46]
+SOBRANCELHA_DIR = [300, 293, 334, 296, 336, 285, 295, 282, 283, 276]
+
+## Lista única com todos os pontos do rosto
+PONTOS_ROSTO = CONTORNO + LABIOS + OLHO_ESQ + OLHO_DIR + SOBRANCELHA_ESQ + SOBRANCELHA_DIR
+
+## Config e detector para vídeo -- MAOS
+options_video_mao = HandLandmarkerOptions(
     base_options=python.BaseOptions(model_asset_path='hand_landmarker.task'),
     running_mode=RunningMode.VIDEO,
     num_hands=1,
     min_hand_detection_confidence=0.5,
     min_tracking_confidence=0.5
     )
-detector_video = HandLandmarker.create_from_options(options_video)
+detector_video_mao = HandLandmarker.create_from_options(options_video_mao)
 
 
-## Config e detector para imagem
-options_estatico = HandLandmarkerOptions(
+## Config e detector para imagem -- MAOS
+options_estatico_mao = HandLandmarkerOptions(
     base_options=python.BaseOptions(model_asset_path='hand_landmarker.task'),
     running_mode=RunningMode.IMAGE,
     num_hands=1,
     min_hand_detection_confidence=0.5,
     )
-detector_imagem = HandLandmarker.create_from_options(options_estatico)
+detector_imagem_mao = HandLandmarker.create_from_options(options_estatico_mao)
+
+## Config e detector para video -- ROSTO
+options_dinamico_rosto = FaceLandmarkerOptions(
+            base_options = python.BaseOptions(model_asset_path = 'face_landmarker_v2_with_blendshapes.task'),
+            running_mode = RunningMode.VIDEO,
+            num_faces = 1,
+            min_face_detection_confidence = 0.5
+        )
+detector_video_rosto = FaceLandmarker.create_from_options(options_dinamico_rosto)
+
+## Config e detector para imagem -- ROSTO
+options_estatico_rosto = FaceLandmarkerOptions(
+            base_options = python.BaseOptions(model_asset_path = 'face_landmarker_v2_with_blendshapes.task'),
+            running_mode = RunningMode.IMAGE,
+            num_faces = 1,
+            min_face_detection_confidence = 0.5
+        )
+detector_imagem_rosto = FaceLandmarker.create_from_options(options_estatico_rosto)
 
 if os.path.isdir(PASTA_DATASET):
     for sinal in os.listdir(PASTA_DATASET):
@@ -62,7 +91,7 @@ if os.path.isdir(PASTA_DATASET):
                         mp_frame = mp.Image(image_format= mp.ImageFormat.SRGB, data=frame_rgb )
 
                         timestamp = int(cap.get(cv2.CAP_PROP_POS_MSEC))
-                        resultado = detector_video.detect_for_video(mp_frame, timestamp)
+                        resultado = detector_video_mao.detect_for_video(mp_frame, timestamp)
 
                         if resultado.hand_landmarks:
                             pontos = []
@@ -127,7 +156,7 @@ if os.path.isdir(PASTA_DATASET):
                     mp_imagem = mp.Image(image_format= mp.ImageFormat.SRGB, data=imagem_rgb )
 
                     ## salva o resultado com os pontos detectados nas mãos
-                    resultado = detector_imagem.detect(mp_imagem)
+                    resultado = detector_imagem_mao.detect(mp_imagem)
 
                     if resultado.hand_landmarks:
                         ## Salva a coordenada de cada ponto
